ee465750f8dad2e313812c8ffbe3409f81e7903a
caijiaqi <caijiaqi2017@gmail.com>
2019-02-21 18:09:12 +0800

    remove battery status

--- dwmstatus.go
+++ dwmstatus.go
@@ -5,7 +5,6 @@ package main
 import "C"
 
 import (
-	"code.google.com/p/gompd/mpd"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,21 +13,6 @@ import (
 )
 
 var dpy = C.XOpenDisplay(nil)
-
-func getBatteryPercentage(path string) (perc int, err error) {
-	energy_now, err := ioutil.ReadFile(fmt.Sprintf("%s/energy_now", path))
-	if err != nil {
-		return -1, err
-	}
-	energy_full, err := ioutil.ReadFile(fmt.Sprintf("%s/energy_full", path))
-	if err != nil {
-		return -1, err
-	}
-	var enow, efull int
-	fmt.Sscanf(string(energy_now), "%d", &enow)
-	fmt.Sscanf(string(energy_full), "%d", &efull)
-	return enow * 100 / efull, nil
-}
 
 func getLoadAverage(file string) (lavg string, err error) {
 	loadavg, err := ioutil.ReadFile(file)
@@ -53,31 +37,24 @@ func main() {
 	if dpy == nil {
 		log.Fatal("Can't open display")
 	}
-	mpdClient, err := mpd.Dial("tcp", "localhost:6600")
+
+	local, err := time.LoadLocation("Local")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
-		t := time.Now().Format("15:04")
-		b, err := getBatteryPercentage("/sys/class/power_supply/BAT1")
+
+		now := time.Now()
+
+		t := now.In(local).Format("Mon Jan _2 15:04:05 2006")
+
+		l, err := getLoadAverage("/proc/loadavg")
 		if err != nil {
 			log.Fatal(err)
 		}
-		status, err := mpdClient.Status()
-		if err != nil {
-			log.Fatal(err)
-		}
-		var np string
-		if status["state"] == "play" || status["state"] == "pause" {
-			attrs, err := mpdClient.CurrentSong()
-			if err != nil {
-				log.Fatal(err)
-			}
-			np = fmt.Sprintf("\x1b[1;31m%s\x1b[0m \x1b[1;37mby\x1b[0m \x1b[1;33m%s\x1b[0m \x1b[1;37mfrom\x1b[0m \x1b[1;34m%s\x1b[0m \x1b[1;30m::\x1b[0m ", attrs["Title"], attrs["Artist"], attrs["Album"])
-		} else {
-			np = ""
-		}
-		s := formatStatus("%s\x1b[1;32m%d%%\x1b[0m \x1b[1;30m::\x1b[0m %s", np, b, t)
+		// fmt.Printf("L: %s | %s", l, t)
+		s := formatStatus("L: %s | %s", l, t)
 		setStatus(s)
 		time.Sleep(time.Second)
 	}
